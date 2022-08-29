c429a7c4eba3043c4ce5f5030a3c32ed8b33c142
DevBadger10 <104037142+DevBadger10@users.noreply.github.com>
2022-08-29 16:43:29 +1000

    rainbowbackround

---
+++ rainbowbackround.js
@@ -0,0 +1,58 @@
+var r = 255
+var g = 255
+var b = 255
+var rand = 0
+
+while (true) {
+    sleep(100)
+    rand = getRandomInt(3)
+    if (rand == 0) {
+        if (r == 255) {
+            r = 254
+        }
+        if (r == 0) {
+            r = 1
+        }
+        rand = getRandomInt(2)
+        if (rand == 0) {
+            r -= 1
+        }
+        if (rand == 1) {
+            r =+ 1
+        }
+    }
+    if (rand == 1) {
+        if (g == 255) {
+            g = 254
+        }
+        if (g == 0) {
+            g = 1
+        }
+        rand = getRandomInt(2)
+        if (rand == 0) {
+            g -= 1
+        }
+        if (rand == 1) {
+            g =+ 1
+        }
+    }
+    if (rand == 2) {
+        if (b == 255) {
+            b = 254
+        }
+        if (b == 0) {
+            b = 1
+        }
+        rand = getRandomInt(2)
+        if (rand == 0) {
+            b -= 1
+        }
+        if (rand == 1) {
+            b =+ 1
+        }
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
